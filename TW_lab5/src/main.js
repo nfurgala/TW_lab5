c234d5c5df259abbfb8f7639aeb4639a3793a692
Natalia Furgała <furgalanatalia@gmail.com>
2026-05-16 21:44:59 +0200

    Wykonano Zadanie 5

---
+++ TW_lab5/src/main.js
@@ -0,0 +1,45 @@
+import dayjs from 'dayjs';
+
+const form = document.getElementById('birthdayForm');
+const bdayInput = document.getElementById('bdayInput');
+const dialog = document.getElementById('resultDialog');
+const closeBtn = document.getElementById('closeBtn');
+const dialogContent = document.getElementById('dialogContent');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const birthDate = dayjs(bdayInput.value);
+    const today = dayjs().startOf('day');
+
+    const daysLived = today.diff(birthDate, 'days');
+
+    let nextBirthday = birthDate.year(today.year());
+    if (today.isAfter(nextBirthday)) {
+        nextBirthday = nextBirthday.add(1, 'year');
+    }
+
+    const isToday = today.format('MM-DD') === birthDate.format('MM-DD');
+
+    const weeksLeft = nextBirthday.diff(today, 'weeks');
+
+    let message = `<p class="mb-4">Od Twojej daty urodzenia minęło: <strong>${daysLived} dni</strong>.</p>`;
+
+
+    if (isToday) {
+        message += `<p class="font-bold text-lg text-green-600">Wszystkiego najlepszego!</p>`;
+    } else {
+        message += `<p>Do najbliższych urodzin pozostało tygodni: <strong>${weeksLeft}</strong>.</p>`;
+        
+        if (weeksLeft === 0) {
+            message += `<p class="font-bold mt-2">Masz urodziny w tym tygodniu!</p>`;
+        }
+    }
+
+    dialogContent.innerHTML = message;
+    dialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+    dialog.close();
+});
---
+++ TW_lab5/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.js",
+  ],
+  theme: {
+    extend: {
+        colors: {
+            'primary': '#660000',
+    },
+  },
+},
+  plugins: [],
+}
+
